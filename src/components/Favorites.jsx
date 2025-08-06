467ebe2d174b8743ba45ea4fd8149ec29a4cd5e3
BenKillerX <benedictakhere802@gmail.com>
2025-08-06 19:44:45 +0100

    Updated movie project

--- src/components/Favorites.jsx
+++ src/components/Favorites.jsx
@@ -3,28 +3,30 @@ import '../css/Favorites.css';
 import { useMovieContext } from '../Context/MovieContext';  
 import MovieCard from './MovieCard';
 const Favorites = () => {
-    const { favorites } = useMovieContext();
-    if(favorites){
-        return (
-            <div className="favorites">
-            <h1>Your Favorites</h1>
-            <div className='favorites-container'>
-                <h2>Favorites Movies</h2>
-                <div className="movies-grid">
-                    {favorites.map(movie => (
-                        <MovieCard key={movie.imdbID} movie={movie} />
-                    ))}
-                </div>
-            </div>
-            </div>
-        );
-    }
-  return (
-    <div className='favorites-empty'>
-        <h2>No Favorites Movies Yet</h2>
-        <p>Start adding to your favorites and they will appear here</p>
-    </div>
-  )
-}
+  const { favorites } = useMovieContext();
 
-export default Favorites
+  if (!favorites || favorites.length === 0) {
+    return (
+      <div className='favorites-empty'>
+        <h2>No Favorite Movies Yet</h2>
+        <p>Please select a movie to add to your favorites.</p>
+      </div>
+    );
+  }
+
+  return (
+    <div className="favorites">
+      <h1>Your Favorites</h1>
+      <div className='favorites-container'>
+        <h2>Favorite Movies</h2>
+        <div className="movies-grid">
+          {favorites.map(movie => (
+            <MovieCard key={movie.imdbID} movie={movie} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Favorites;
